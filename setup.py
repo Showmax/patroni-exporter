4a31bbb62a4f7955e1e91e1d5701692916175406
alexandrovas <alex.mirenki@gmail.com>
2021-07-06 12:58:27 +0300

    pip packaging notes

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup
 
 setup(
     name='patroni_exporter',
-    version='0.0.1',
+    version='0.0.2',
     description='Export Patroni metrics in Prometheus format',
+    url='https://github.com/Showmax/patroni-exporter',
     author='Jan Tomsa',
     author_email='ops@showmax.com',
     scripts=['patroni_exporter.py'],
+    install_requires=['prometheus_client','python-dateutil','requests'],
 )
